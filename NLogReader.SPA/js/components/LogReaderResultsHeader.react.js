27f7d2546fd015076828199b45cf8681720afec9
Dan Esparza <esparza.dan@gmail.com>
2017-03-01 13:02:47 -0500

    Revert "Started adding charts"
    This reverts commit 9b5d9afec5fe204395dcf8f772a05ffd82b11e10.

--- NLogReader.SPA/js/components/LogReaderResultsHeader.react.js
+++ NLogReader.SPA/js/components/LogReaderResultsHeader.react.js
@@ -1,5 +1,4 @@
 import {Component} from 'react';
-import {AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
 
 //  The API utils
 import LogReaderAPIUtils from '../utils/LogReaderAPIUtils';
@@ -7,16 +6,6 @@ import LogReaderAPIUtils from '../utils/LogReaderAPIUtils';
 //  The stores
 import LogStore from '../stores/LogStore';
 import ConfigStore from '../stores/ConfigStore';
-
-const data = [
-      {name: 'Page A', error: 4000, warn: 2400, debug: 2400},
-      {name: 'Page B', error: 3000, warn: 1398, debug: 2210},
-      {name: 'Page C', error: 2000, warn: 9800, debug: 2290},
-      {name: 'Page D', error: 2780, warn: 3908, debug: 2000},
-      {name: 'Page E', error: 1890, warn: 4800, debug: 2181},
-      {name: 'Page F', error: 2390, warn: 3800, debug: 2500},
-      {name: 'Page G', error: 3490, warn: 4300, debug: 2100},
-];
 
 class LogReaderResultsHeader extends Component {
 
@@ -61,19 +50,7 @@ class LogReaderResultsHeader extends Component {
     {
       return (
         <div id='results-header'>
-          <div>
-            Showing <b>{this.state.itemcount}</b> log items out of <b>{this.state.totalcount}</b> <button type='button' className='btn btn-success btn-xs' onClick={this._moreClick}>Show more</button>
-          </div>
-          <AreaChart width={600} height={100} data={data}
-                margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-            <XAxis dataKey="name"/>
-            <YAxis/>
-            <CartesianGrid strokeDasharray="3 3"/>
-            <Tooltip/>
-            <Area type='monotone' dataKey='error' stackId="1" stroke='#C70039' fill='#C70039' />
-            <Area type='monotone' dataKey='warn' stackId="1" stroke='#FF5733' fill='#FF5733' />
-            <Area type='monotone' dataKey='debug' stackId="1" stroke='#FFC300' fill='#FFC300' />
-          </AreaChart>
+          Showing <b>{this.state.itemcount}</b> log items out of <b>{this.state.totalcount}</b> <button type='button' className='btn btn-success btn-xs' onClick={this._moreClick}>Show more</button>
         </div>
       );  
     }
@@ -81,19 +58,7 @@ class LogReaderResultsHeader extends Component {
     {
        return (
         <div id='results-header'>
-          <div>
-            Showing <b>{this.state.itemcount}</b> log items
-          </div>
-          <AreaChart width={600} height={100} data={data}
-                margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-            <XAxis dataKey="name"/>
-            <YAxis/>
-            <CartesianGrid strokeDasharray="3 3"/>
-            <Tooltip/>
-            <Area type='monotone' dataKey='error' stackId="1" stroke='#C70039' fill='#C70039' />
-            <Area type='monotone' dataKey='warn' stackId="1" stroke='#FF5733' fill='#FF5733' />
-            <Area type='monotone' dataKey='debug' stackId="1" stroke='#FFC300' fill='#FFC300' />
-          </AreaChart>
+          Showing <b>{this.state.itemcount}</b> log items
         </div>
       );   
     }
